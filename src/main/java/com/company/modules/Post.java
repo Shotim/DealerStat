468501e7d0455c1aef98e97d55ad5a63df918b88
Shotim <39776975+Shotim@users.noreply.github.com>
2019-09-10 00:38:59 +0300

    added new Post class

---
+++ src/main/java/com/company/modules/Post.java
@@ -0,0 +1,16 @@
+package com.company.modules;
+
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
+public class Post {
+    Integer id;
+    Integer dealer_id;
+}
